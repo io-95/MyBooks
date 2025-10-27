c952acec13a8b852587c2fca0413e17bcde586fe
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-27 12:10:40 +0100

    add all books endpoint

--- Backend/MyBooks/src/main/java/com/example/mybooks/Controller/BookController.java
+++ Backend/MyBooks/src/main/java/com/example/mybooks/Controller/BookController.java
@@ -1,7 +1,13 @@
 package com.example.mybooks.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class BookController {
+    @GetMapping("/books")
+    List<Book> all(){
+        return;
+    }
 }
