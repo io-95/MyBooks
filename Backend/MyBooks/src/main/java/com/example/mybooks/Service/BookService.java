f4cd0b547fa970eac8f90ce3535b67a1a5518268
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-27 16:04:23 +0100

    implement findAll method in BookService

--- Backend/MyBooks/src/main/java/com/example/mybooks/Service/BookService.java
+++ Backend/MyBooks/src/main/java/com/example/mybooks/Service/BookService.java
@@ -1,11 +1,19 @@
 package com.example.mybooks.Service;
 
 import com.example.mybooks.Interface.BookRepository;
+import com.example.mybooks.Model.Book;
+import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
