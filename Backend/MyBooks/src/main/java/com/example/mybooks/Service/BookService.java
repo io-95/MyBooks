a4ab9a3afa58b4d72f07a7b4360ca1c1076e859a
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-27 16:02:35 +0100

    implement BookService with service and autowired annotations, delete class keyword in bookRepository

--- Backend/MyBooks/src/main/java/com/example/mybooks/Service/BookService.java
+++ Backend/MyBooks/src/main/java/com/example/mybooks/Service/BookService.java
@@ -1,4 +1,11 @@
 package com.example.mybooks.Service;
 
+import com.example.mybooks.Interface.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
 }
