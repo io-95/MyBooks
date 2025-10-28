63b22716d7ea41f2d07f4e73bea882da7ab3a8b8
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-28 15:24:21 +0100

    add search bar

--- Frontend/MyBooks-Frontend/src/app/pages/home/home.component.ts
+++ Frontend/MyBooks-Frontend/src/app/pages/home/home.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [MatIconModule, MatButtonModule]
+  imports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class HomeComponent {
