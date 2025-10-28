65754b664b661fe4636ea7741f303484fae085d8
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-28 22:04:36 +0100

    implement form html and css

--- Frontend/MyBooks-Frontend/src/app/app.routes.ts
+++ Frontend/MyBooks-Frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FormComponent } from './pages/form/form.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    //{ path: 'form', component: FormComponent }
+    { path: 'form', component: FormComponent }
   ];
