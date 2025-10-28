bdeba567f969767fa758c84424f58c8360fdfc78
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-28 16:56:41 +0100

    add book service

---
+++ Frontend/MyBooks-Frontend/src/app/service/book.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  publishingYear: number;
+  isbn: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class BookService {
+  private apiUrl = 'http://localhost:8080/api/books';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  deleteBook(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
