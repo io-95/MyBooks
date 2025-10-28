bf6fe2d0f9ee6c58bef2b112b49328e30efeaf17
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-28 12:12:01 +0100

    add icon to header, change size of page header to medium

--- Frontend/MyBooks-Frontend/src/app/components/header/header.component.ts
+++ Frontend/MyBooks-Frontend/src/app/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [MatToolbarModule, MatIconModule]
+  imports: [MatToolbarModule]
 })
 
 export class HeaderComponent {
