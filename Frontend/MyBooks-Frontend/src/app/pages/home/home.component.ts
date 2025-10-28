9e34d9f980b01c526cafdc3d66908cf4eca72903
io-171737 <ibrahim.oezel@hs-weingarten.de>
2025-10-28 14:32:53 +0100

    add header, and add new book button

--- Frontend/MyBooks-Frontend/src/app/pages/home/home.component.ts
+++ Frontend/MyBooks-Frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  imports: [MatIconModule, MatButtonModule]
 })
 
 export class HomeComponent {
