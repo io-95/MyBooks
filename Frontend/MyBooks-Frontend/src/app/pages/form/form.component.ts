7bd8a46394cd29cd18553f06fd78b8e1e955deeb
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-29 17:51:46 +0100

    implement cancel button to switch back to landing page

--- Frontend/MyBooks-Frontend/src/app/pages/form/form.component.ts
+++ Frontend/MyBooks-Frontend/src/app/pages/form/form.component.ts
@@ -3,6 +3,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -12,5 +13,9 @@ import {MatButtonModule} from '@angular/material/button';
 })
 
 export class FormComponent {
+  constructor(private router: Router) {}
 
+  switchToLandingPage(){
+    this.router.navigate(['']);
+  }
 }
