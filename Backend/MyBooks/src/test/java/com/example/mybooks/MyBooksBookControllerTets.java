38d2fbe2385762c157611dc3b38b896fe16e5943
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-31 15:13:50 +0100

    implement a test for GET /books endpoint

---
+++ Backend/MyBooks/src/test/java/com/example/mybooks/MyBooksBookControllerTets.java
@@ -0,0 +1,34 @@
+package com.example.mybooks;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MyBooksBookControllerTets {
+    
+    @Autowired
+	private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnBooksInDb() throws Exception{
+        this.mockMvc.perform(get("/books")).andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.length()").value(5))
+                        .andExpect(jsonPath("$[*].id").exists())
+                        .andExpect(jsonPath("$[*].title").exists())
+                        .andExpect(jsonPath("$[*].author").exists());
+    }
+    
+}
