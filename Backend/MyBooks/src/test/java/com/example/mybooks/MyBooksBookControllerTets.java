754376c365e71ee57d41229d7c2982d820d0e469
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-31 15:33:28 +0100

    delete unused import in MyBookBookControllerTests

--- Backend/MyBooks/src/test/java/com/example/mybooks/MyBooksBookControllerTets.java
+++ Backend/MyBooks/src/test/java/com/example/mybooks/MyBooksBookControllerTets.java
@@ -2,7 +2,6 @@ package com.example.mybooks;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
